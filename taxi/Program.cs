a70862c84f54fd72473c8c921033ed8188f05bed
trynxID <sidiq.firman48@gmail.com>
2022-03-25 10:28:45 +0700

    taxi-app

---
+++ taxi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek taxi
+            Taxi taxi1 = new Taxi();
+
+            //pengesetan nilai properties
+            taxi1.Drivername = "sidiq";
+            taxi1.Onduty = true;
+            taxi1.NumPassenger = 4484;
+
+            //pemanggilan method
+            taxi1.TaxiInfo();
+            taxi1.PickUpPassenger();
+            taxi1.DropOffPassenger();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi/taxi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    public class Taxi
+    {
+        //properties
+        public string Drivername { get; set; }
+        public bool Onduty { get; set; }
+        public int NumPassenger { get; set; }
+        
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Drivername : {0}", Drivername);
+            Console.WriteLine("Onduty : {0}", Onduty);
+            Console.WriteLine("NumPassenger : {0}\n", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang",Drivername);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang",Drivername);
+        }
+    }
+}
